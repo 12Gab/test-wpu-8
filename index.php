51090f3a61dcb698cd7b3e05f52c7ecdcc9f5bdb
g1ab <bie.ritan112@gmail.com>
2023-01-12 01:42:24 +0700

    tambah file

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>hissssss</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut ducimus officia deleniti, voluptates nostrum consectetur aliquam voluptatibus maiores error ad corrupti assumenda expedita aliquid a est magni omnis dignissimos modi!</p>
+    <li>tast</li>
+</body>
+</html>
